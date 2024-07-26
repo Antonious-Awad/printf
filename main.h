ea887ca41aeffc035f20dfdd240b34eefe804423
Antonious-Awad <antonuostony1@gmail.com>
2024-07-26 23:10:19 +0300

    chore: printf implementation

--- main.h
+++ main.h
@@ -4,10 +4,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct format_specifier - defines a format specifier along
+ * with its corresponding conversion function
+ * @specifier: specifier string
+ * @conversion_func: function that converts the specifier
+ */
+
+typedef struct format_specifier
+{
+	char *specifier;
+	int (*conversion_func)(va_list);
+} fs;
+
 int _putchar(char);
 int _print_str(va_list);
 int _strlen(char *);
-int _print_percent(void);
+int _print_percent(va_list);
 int _printf(const char *format, ...);
 
 #endif
