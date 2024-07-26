352930446ff8ad33315071c42a7d84faff98658f
Antonious-Awad <antonuostony1@gmail.com>
2024-07-26 17:09:08 +0300

    feat: implement _print_percent

---
+++ _print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_percent - prints the percentage sign .
+ *
+ * Return: return value of _putchar (i.e 1 or -1).
+ */
+
+int _print_percent(void)
+{
+	char c = '%';
+
+	return (_putchar(c));
+}
