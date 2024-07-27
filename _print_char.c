ceabd044131e6d035ceb04604437e71e0c984bfc
Antonious-Awad <antonuostony1@gmail.com>
2024-07-27 15:09:46 +0300

    fix: code problems
    Co-authored-by: GeorgeSamir10 <georgesamir3500@gmail.com>

--- _print_char.c
+++ _print_char.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* _print_char - prints a charachter
-* @argPtr: argument pointer
-* Return: 1 (success)
+ * _print_char - prints a character
+ * @argPtr: argument pointer
+ * Return: 1 (success)
  */
 
 int _print_char(va_list argPtr)
 {
 	char c;
 
-	c = va_arg(argptr, int);
-	_putchar(c)
+	c = va_arg(argPtr, int);
+	_putchar(c);
 	return (1);
 }
