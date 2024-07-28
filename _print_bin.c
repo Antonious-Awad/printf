5883055d9497329dc3747110903113c55b00a49b
Antonious-Awad <antonuostony1@gmail.com>
2024-07-28 03:02:38 +0300

    fix: edge case

--- _print_bin.c
+++ _print_bin.c
@@ -8,11 +8,11 @@
 int _print_bin(va_list argPtr)
 {
 	unsigned int temp, num;
-	int len = 0, i = 0, bin, counter = 0;
+	int len = 0, i, bin, counter;
 	char *p;
 
 	num = va_arg(argPtr, unsigned int);
-	if (!num)
+	if (num == 0)
 		_putchar('0');
 	temp = num;
 
@@ -27,16 +27,16 @@ int _print_bin(va_list argPtr)
 	if (!p)
 		return (-1);
 
+	i = len;
+	p[i--] = '\0';
 	while (num > 0)
 	{
 		bin = num % 2;
-		p[i++] = bin + '0';
+		p[i--] = bin + '0';
 		num /= 2;
 	}
-	p[i] = '\0';
 
-	for (i = (len - 1); i >= 0; i--)
-		counter += _putchar(p[i]);
+	counter = _puts(p);
 	free(p);
 	return (counter);
 }
