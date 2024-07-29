fb41713fe93493a77a526f386f895b569a23e296
Antonious-Awad <antonuostony1@gmail.com>
2024-07-30 00:23:10 +0300

    feat: print_int fix

--- _printf_int.c
+++ _printf_int.c
@@ -5,60 +5,55 @@
  * @f: pointer to flag struct
  * Return: 1(for add flag)
  */
-
 int add_flag(flag *f)
 {
 	if (f->plus)
-		_putchar('+');
+		return (_putchar('+'));
 	else if (f->space)
-		_putchar(' ');
-	return (1);
+		return (_putchar(' '));
+	return (0);
 }
+
 /**
  * _print_int - Function to print integer
  * @argPtr: argument Pointer
  * @f: pointer to flag struct
  * Return: counter(number of printed characters)
  */
-
 int _print_int(va_list argPtr, flag *f)
 {
-	int n = va_arg(argPtr, int), lens = 0, ext, i, numPrnt, count = 0, num;
+	int n = va_arg(argPtr, int), count = 0, i = 0;
+	unsigned int num;
+	char buffer[11]; /* INT_MAX has 10 digits, plus null terminator */
 
-	num = n;
-	if (n >= 0)
-		count += add_flag(f);
-	if (n != 0)
+	if (n == 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			count++;
-		}
-		while (num != 0)
-		{
-			num /= 10;
-			lens++;
-		}
-		ext = 1;
-		for (i = 1; i <= lens - 1; i++)
-			ext *= 10;
-		for (i = 1; i <= lens; i++)
-		{
-			numPrnt = n / ext;
-			if (n < 0)
-				_putchar((numPrnt * -1) + '0');
-			else
-				_putchar(numPrnt + '0');
-			count++;
-			n = n - (numPrnt * ext);
-			ext /= 10;
-		}
+		count += add_flag(f);
+		return (count + _putchar('0'));
+	}
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = (unsigned int)(-n);
 	}
 	else
 	{
-		_putchar('0');
-		return (1 + (f->plus || f->space));
+		count += add_flag(f);
+		num = (unsigned int)n;
 	}
+
+	/* Store digits in reverse order */
+	while (num > 0)
+	{
+		buffer[i++] = num % 10 + '0';
+		num /= 10;
+	}
+
+	while (i > 0)
+	{
+		count += _putchar(buffer[--i]);
+	}
+
 	return (count);
 }
