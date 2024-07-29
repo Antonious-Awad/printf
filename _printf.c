7c2fd352e14c9d8daae8b4949c73d318a5e0eb64
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-07-29 04:56:16 +0300

    editing the printf.c

--- _printf.c
+++ _printf.c
@@ -13,9 +13,10 @@ int _printf(const char *format, ...)
 			{"%s", _print_str}, {"%%", _print_percent}, {"%c", _print_char},
 			{"%d", _print_int}, {"%i", _print_int}, {"%b", _print_bin},
 			{"%X", _print_HEX}, {"%x", _print_hex}, {"%p", _print_ptr},
-			{"%o", _print_oct}, {"%u", _print_unsigned}};
+			{"%o", _print_oct}, {"%u", _print_unsigned}, {"%S", _print_custstr}
+			};
 	/*Length of convert struct*/
-	int convert_len = 11, i, j, output_len = 0, isSpecifier;
+	int convert_len = 12, i, j, output_len = 0, isSpecifier;
 	va_list argPtr;
 
 	va_start(argPtr, format);
