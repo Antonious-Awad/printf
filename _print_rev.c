9de07da4ad4943253313b6c0f05246a51cac29ea
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-01 00:23:28 +0300

    Edit: print_rev V3

--- _print_rev.c
+++ _print_rev.c
@@ -11,33 +11,25 @@ int _print_rev(va_list argPtr, flag *f)
 {
 	char *str;
 	char *rev_str;
-	int len = 0, i, j;
+	int len = 0, i;
 
 	(void)f;
 	str = va_arg(argPtr, char*);
 
-	if (!str)
-	{
+	if (str == NULL)
 		str = "(null)";
-		return (0);
-	}
+
 	rev_str = malloc(sizeof(char) * len + 1);
 	_strcpy(rev_str, str);
-	len = _strlen(rev_str);
 
 	if (!rev_str)
 		return (-1);
 
+	while (rev_str[len] != '\0')
+		len++;
 
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		char temp = rev_str[i];
-
-		rev_str[i] = rev_str[j];
-		rev_str[j] = temp;
-	}
-
-	_puts(rev_str);
+	for (i = len - 1;  i >= 0; i--)
+		_putchar(rev_str[i]);
 	free(rev_str);
 
 	return (len);
